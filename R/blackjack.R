3d70f721be509e94d79b9553622a938b43d03b85
Louis-Thomas101 <louis142thomas@gmail.com>
2024-12-13 13:18:52 +0000

    needed to randomize the deck, and need to feed in the shuffled_deck data in to the draw_card function

--- R/blackjack.R
+++ R/blackjack.R
@@ -13,16 +13,17 @@ blackjack <- function()
 {
   timer <- 1
 
-  shuffled_deck <- create_deck()
+  shuffled_deck <- sample(create_deck())
+  #shuffled_deck <- load_data()
   card_index <- 0
-  player <- list(draw_card(timer))
+  player <- list(draw_card(timer, shuffled_deck))
     timer <- timer + 1
-    player[[2]] <- draw_card(timer)
+    player[[2]] <- draw_card(timer, shuffled_deck)
       timer <- timer+1
 
-  dealer <- list(draw_card(timer))
+  dealer <- list(draw_card(timer, shuffled_deck))
     timer <- timer+1
-    dealer[[2]] <- draw_card(timer)
+    dealer[[2]] <- draw_card(timer, shuffled_deck)
       timer <- timer+1
 
   while(TRUE)
@@ -39,7 +40,7 @@ blackjack <- function()
 
     if(var == "Hit" || var == "hit")
     {
-      player <- append(player, list(draw_card()))
+      player <- append(player, list(draw_card(timer, shuffled_deck)))
         timer <- timer + 1
       print(player)
       value <- countValues(player)
@@ -84,7 +85,7 @@ blackjack <- function()
     #be higher up
     if(countValues(dealer) < 17)
     {
-      dealer <- append(dealer, list(draw_card()))
+      dealer <- append(dealer, list(draw_card(timer, shuffled_deck)))
         timer <- timer + 1
     }
 
