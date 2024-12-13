52151e3f7f4cb9abb3d3fb26b18cce2f04d06976
AoifeSteenson <aoifesteenson@outlook.com>
2024-12-13 13:20:31 +0000

    stashing

--- R/blackjack.R
+++ R/blackjack.R
@@ -6,6 +6,8 @@
 #' the player chose Stick, the player's and dealer's cards will the summed up and compared to find the winner of the game. Once the game is over the player
 #' is given the chose to play again.
 #'
+#'@export
+#'
 #' @examples
 #' blackjack()
 #'
