485e2ba45e733f2aa0f9d7a7fe73895d7b251008
Louis-Thomas101 <louis142thomas@gmail.com>
2024-12-18 12:08:12 +0000

    There were two functions for some reason. My mistake, should only be one.

--- R/print_card.R
+++ R/print_card.R
@@ -31,18 +31,3 @@ print.card <- function(object) {
   ")
   }
 }
-
-print.card <- function(object) {
-  if(class(object) == "card"){
-    test <- " "
-    suit <- switch(object$suit, "spade" = " ♠ ", "heart" = " ♥ ", "club" = " ♣ ", "diamond" = " ♦ ", object$suit)
-    face <- switch(object$face, "queen" = " Q ", "king" = " K ",
-                   "jack" = " J " , "ace" = " A ", "10" = " 10", paste0(" ", object$face," "))
-    cat("
-  # # # #
-  #",suit,"#
-  #",face,"#
-  # # # #
-  ")
-  }
-}
