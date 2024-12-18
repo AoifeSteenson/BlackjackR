0cdfb945518b85be14ee0cf7eedbe40b45f5667b
Louis-Thomas101 <louis142thomas@gmail.com>
2024-12-18 17:11:12 +0000

    Checking for problematic parameters in the create_card function

--- R/create_card.R
+++ R/create_card.R
@@ -1,5 +1,14 @@
 create_card <- function(face = c(1:13), suit = c("spade", "heart", "club", "diamond")) {
-  sample_data <- list()
+
+  if(face >= 13 & face <= 1) {
+    warning("Out of bounds exceptions")
+    face <- 1
+  }
+  suit <- tolower(suit)
+  if(suit %in% c("spade", "heart", "club", "diamond") == FALSE){
+    warning("Not in the suit options")
+    suit <- "spade"
+  }
 
   face <- as.vector(as.character(face))
   face <- switch(face[1], "1"="ace", "11"="jack", "12"="queen", "13"="king", face)
